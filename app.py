1d20b450e096070cce19f1b9dbf2f9672ed4895c
jiyaa <jiyaadramc@gmail.com>
2026-03-30 23:27:10 +0530

    updated hobbies

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Name: Jiyaa Ramchandani | AppID: 12345"
+    return "Name: Jiyaa | AppID: 12345 | Hobbies: Music, Travel"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
